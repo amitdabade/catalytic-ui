af2f39f64930a7005360a9e9fec1354cb3337e55
Amit Dabade <31438237+amitdabade@users.noreply.github.com>
2018-01-12 18:41:20 +0530

    Add files via upload

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,16 @@
 var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
 var sass        = require('gulp-sass');
+var cleanCSS    = require('gulp-clean-css');
+var uglify      = require('gulp-uglify');
+var rename      = require("gulp-rename");
 var reload      = browserSync.reload;
 
 // Static server
 gulp.task('browser-sync', function() {
     return browserSync.init({
         server: {
-            baseDir: "./src/"
+            baseDir: "./code/"
         },
         port:8888
     });
@@ -18,30 +21,51 @@ gulp.task('bs-reload', function() {
   browserSync.reload();
 });
 
-//SASS to css
+//SCSS to css
 gulp.task('sass', function() {
-  return gulp.src('./src/*.scss')
-    // .pipe(sourcemaps.init())
+  return gulp.src('./code/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./src/'))
+    .pipe(gulp.dest('./code/'))
     .pipe(reload({
       stream: true
     }));
 });
 
+//minify css
+gulp.task('minify-css', () => {
+  return gulp.src('./code/*.css')
+    .pipe(cleanCSS({compatibility: 'ie8',keepBreaks: true}))
+    .pipe(rename({
+            suffix: '.min'
+        }))
+    .pipe(gulp.dest('src'));
+});
 
 
 
+//minify js
+gulp.task('compress', function () {
+  return gulp.src('./code/*.js')
+    .pipe(uglify())
+    .pipe(rename({
+        suffix: '.min'
+    }))
+    .pipe(gulp.dest('src'));
+});
 
-
-
+//make build code
+gulp.task('build', function() {
+    'sass',
+    'minify-css',
+    'compress';
+});
 
 
 //Default task
 //watch file and reload browser
-gulp.task('default', ['browser-sync', 'sass'], function(){
-  gulp.watch(['./src/*.scss'], ['sass']);
-  gulp.watch(['./src/*.html'], ['bs-reload']);
-  gulp.watch(['./src/*.js'], ['bs-reload']);
-  gulp.watch(['./src/*.css'], ['bs-reload']);
+gulp.task('default', ['browser-sync', 'sass','minify-css','compress'], function(){
+  gulp.watch(['./code/*.scss'], ['sass']);
+  gulp.watch(['./code/*.html'], ['bs-reload']);
+  gulp.watch(['./code/*.js'], ['bs-reload']);
+  gulp.watch(['./code/*.css'], ['bs-reload']);
 });
